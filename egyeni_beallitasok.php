a9a91f672293868ac981efa00cfbb900755aa400
Szabó Gergő <gary89@windowslive.com>
2018-04-18 22:23:33 +0200

    0.5

---
+++ egyeni_beallitasok.php
@@ -0,0 +1,10 @@
+<?
+/// Ezeket módosítsd
+define('IP', '192.168.0.0');
+define('PORT', 5000);
+define('USER', 'user');
+define('PASS', 'pass');
+
+// milyen időközönként töltődjön újra az oldal másodpercben kifejezve
+// 5 perc => 5 * 60
+define('UJRATOLTES', 5 * 60);
---
+++ index.php
@@ -0,0 +1,145 @@
+<?php
+include 'egyeni_beallitasok.php';
+?>
+<html> 
+<head>	 
+	<title>Download Station Monitor</title>
+	<meta http-equiv=Content-Type content="text/html; charset=UTF-8">
+	<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+	<meta http-equiv="refresh" content="<?=UJRATOLTES?>">
+</head>
+<body>
+	<div class="right">
+		Az oldal automatikusan frissül<br>
+		Utolsó oldal betöltése: <?=date('H:i:s')?>
+	</div>
+	<h2 class="center"><i>Download Station Monitor</i></h2>	
+	<table id="maintable" class="center">
+		<tr>
+			<th><i>Torrent Neve</i></th>
+			<th><i>Méret</i></th>
+			<th><i>Letöltve</i></th>
+			<th><i>Feltöltve</i></th>
+			<th><i>Folyamat</i></th>
+			<th><i>Átlag</i></th>
+			<th><i>Le</i></th>
+			<th><i>Fel</i></th>
+			<th><i>Tracker</i></th>
+			<th><i>S/L</i></th>
+			<th><i>Áll.</i></th>
+		</tr>
+<?
+	define('MB', 1024 * 1024);
+	define('GB', MB * 1024);
+
+	 
+	$loginUrl='http://'.IP.':'.PORT.'/webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login'.
+		'&account='.USER.'&passwd='.PASS.'&session=DownloadStation&format=cookie';
+	$decodedlogin=json_decode(file_get_contents($loginUrl), true);
+	 
+	define('SID', $decodedlogin['data']['sid']);
+
+	$tasksUrl='http://'.IP.':'.PORT.'/webapi/DownloadStation/task.cgi?api='.
+		'SYNO.DownloadStation.Task&version=1&method=list&_sid='.SID.
+		'&additional=transfer,detail,tracker'; 
+		
+	$decodedrequest=json_decode(file_get_contents($tasksUrl), true);
+	$totaldownloads=$decodedrequest['data']['total']; //get total number of downloads (for statistics)
+
+	usort($decodedrequest['data']['tasks'], function($a, $b) {
+		if($a['status'] == $b['status']) {
+			return strcmp($a['title'], $b['title']);
+		}
+		
+		return strcmp($a['status'], $b['status']);
+	});
+	
+	foreach($decodedrequest['data']['tasks'] as $task) {
+		
+		$title = $task['title'];
+		$size = (float)$task['size'];
+		$status = $task['status'];
+		
+		$size_downloaded = (float)$task['additional']['transfer']['size_downloaded'];
+		$size_uploaded = (float)$task['additional']['transfer']['size_uploaded'];
+		$speed_download = (float)$task['additional']['transfer']['speed_download'];
+		$speed_upload = (float)$task['additional']['transfer']['speed_upload'];
+		$ratio = round($size_uploaded / ($size_downloaded > 0 ? $size_downloaded : 1), 2);
+		
+		$size = round($size / GB,2);
+		$size_downloaded = round($size_downloaded / GB, 2);
+		$size_uploaded = round($size_uploaded / GB, 2);
+		$speed_download = round($speed_download / MB, 2);
+		$speed_upload = round($speed_upload / MB, 2);
+		 
+		$progress = round($size_downloaded /($size > 0 ? $size : 1) * 100, 1);
+		 
+		$trackerstatuses = array();
+		if(isset($task['additional']['tracker'])) {
+			$tracker = $task['additional']['tracker'];
+		   
+			if(is_array($tracker)) {
+				foreach($tracker as $t) {
+					if(isset($t['status']) && strlen($t['status']))
+						$trackerstatuses[] = $t['status'];
+				}
+			}
+			else {
+				try {
+					$trackerstatuses[] = $tracker[0]['status'];
+				}
+				catch (Exception $ex) {
+				}
+			}
+		}
+		$trackerstatus = implode(',', $trackerstatuses);
+		
+		$connectedSeeders = (float)$task['additional']['detail']['connected_seeders'];
+		$connectedLeechers = (float)$task['additional']['detail']['connected_leechers'];
+		
+?>
+			<tr>
+				<td><?=$title?></td>
+				<td class="right size"><?=$size?> GB</td>
+				<td class="right"><?=$size_downloaded?> GB</td>
+				<td class="right"><?=$size_uploaded?> GB</td> 
+				<td>
+					&nbsp;<?=$progress?>%&nbsp;
+					<progress value="<?=$size_downloaded?>" max="<?=$size?>" />
+				</td> 
+				<td class="right"><?=$ratio?></td> 
+				<td class="right size"><?=$speed_download?> MB/s</td>
+				<td class="right size"><?=$speed_upload?> MB/s</td>
+				<td class="center"><?=$trackerstatus?></td>
+				<td class="center"><?=$connectedSeeders?>/<?=$connectedLeechers?></td>
+				<td class="center">
+					<img src="images_status/<?=$status?>.png" width=15 height=15 align="center">
+				</td>
+			</tr>
+<?
+	}
+	
+	$speedsUrl = 'http://'.IP.':'.PORT.'/webapi/DownloadStation/statistic.cgi?api='.
+		'SYNO.DownloadStation.Statistic&version=1&method=getinfo&_sid='.SID;
+	$decodedspeeds = json_decode(file_get_contents($speedsUrl),true);
+	$totaldownspeed = $decodedspeeds['data']['speed_download'] / MB;
+	$totalupspeed = $decodedspeeds['data']['speed_upload'] / MB;
+  
+?>
+	</table>
+	<br>
+	<table class="center">
+		<th colspan="6"><i>Statisztika</i></th>
+		<tr>
+			<td class="autowidth"><b>Összes torrent:</b></td>
+			<td class="autowidth"><?=$totaldownloads?></td>
+			<td class="autowidth"><b>Le seb.:</b></td>
+			<td class="autowidth"><?=round($totaldownspeed, 2)?> MB/s</td>
+			<td class="autowidth"><b>Fel seb.:</b></td>
+			<td class="autowidth"><?=round($totalupspeed, 2)?> MB/s</td>
+		</tr>
+	</table> 
+<footer>
+Verzió: 0.5
+</footer> 
+</html>
